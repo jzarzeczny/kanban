a060446f5189ba089171e863fd1dda4f538cf860
jzarzeczny <jkzarzeczny@gmail.com>
2022-01-13 12:48:46 +0100

    ts refactor step 4

--- client/src/Task.ts
+++ client/src/Task.ts
@@ -1,41 +1,38 @@
 import { Service } from "./Service";
 
-interface UpdateItem{
-    id: string;
+interface UpdateItem {
+    _id: string;
     content: string;
-    position: string;
-}
-
-interface DragEvent<T = Element> extends MouseEvent<T, DragEvent>{
-
+    position?: string;
 }
 
 class Task {
-    static onDragStart(ev: DragEvent){
-        ev.dataTransfer.setData("text/plain", ev.target.id) as DataTransfer
+    static onDragStart(ev: DragEvent) {
+        const element = ev.target as HTMLElement;
+        ev.dataTransfer?.setData("text/plain", element.id);
     }
 
-    static editCard(event:) {
-        event.preventDefault();
-        const elementID = event.target.parentElement.id;
-        const element = document.getElementById(elementID);
-        const editableDiv:Element | undefined = element?.children[1]
+    static editCard(event: Event) {
+        const eventElement = event.target as HTMLElement;
+        const id: string = eventElement.parentElement?.id;
+        const element = document.getElementById(id) as HTMLElement;
+        const editableDiv: HTMLElement = element?.children[1];
         editableDiv.onblur = () => {
             const service = new Service();
-            const objToUpdate:UpdateItem = {
-                id: element?.id,
+            const objToUpdate: UpdateItem = {
+                _id: element.id,
                 content: element?.children[1].innerHTML,
             };
             service.updateItem(objToUpdate);
-        }
+        };
     }
 
-    static removeCard(event:Event):void {
+    static removeCard(event: Event): void {
         event.preventDefault();
         const service = new Service();
-        const target = event.target as HTMLElement
+        const target = event.target as HTMLElement;
         const card = target.parentElement as HTMLElement;
-        card?.parentElement?.removeChild(document.getElementById(card.id));
+        card?.parentElement?.removeChild(document.getElementById(card.id) as HTMLElement);
         service.delateItem(card.id);
     }
 }
